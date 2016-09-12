ffe2ed484e6e386a0bdc0dd7bb7c4f1efbfe0d01
Aleksi Rasio <aleksi.rasio@metropolia.fi>
2016-09-12 10:32:27 +0300

    Preliminary project structure setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,8 +6,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app_server/routes/index');
+//var users = require('./routes/users');
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -23,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routesApi);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
