30e1f1c2c062be7c91bb6fa710304983e3e6e05d
FPulla <joel.vainikka@gmail.com>
2016-09-15 16:50:34 +0300

    added use strict to few files

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*jslint node: true */
+'use strict';
+
 require('dotenv').load();
 var express = require('express');
 var path = require('path');
@@ -30,7 +33,7 @@ app.use('/', routes);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -41,7 +44,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -52,7 +55,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
