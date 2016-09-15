a4f6ddcc1eda562f4a9198fc419d493ae23acf98
Gusgård <gusoskar@hotmail.com>
2016-09-15 14:54:11 +0300

    team model

---
+++ app_api/models/team.js
@@ -0,0 +1,41 @@
+/*jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var teamSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    ],
+    pastUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    ],
+    additionalInfo: {
+      type: String
+    },
+    teamStats: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'tournament'
+        //and somehow the result
+      }
+    ],
+    registerDate: {
+      type: Date,
+      required: true
+    }
+  }
+);
+
+mongoose.model('Team', teamSchema, 'teams');
