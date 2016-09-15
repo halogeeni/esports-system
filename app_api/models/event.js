53a343c11a6e46692ca9095b4b4a7225ff876a52
Joona <vainikka.joona@gmail.com>
2016-09-15 14:53:21 +0300

    Added event model

---
+++ app_api/models/event.js
@@ -0,0 +1,48 @@
+/*jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var eventSchema = new mongoose.Schema(
+  {
+    eventName: {
+      type: String,
+      required: true
+    },
+    eventInfo: {
+      type: String,
+      required: true
+    },
+    eventContactInfo: {
+      type: String,
+      required: true
+    },
+    eventTournaments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tournament'
+      // -----
+      }
+    ],
+    eventSponsors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Sponsor'
+      }
+    ],
+    eventCreationDate: {
+      type: Date,
+      required: true
+    },
+    eventStartDate: {
+      type: Date,
+      required: true
+    },
+    eventEndDate: {
+      type: Date,
+      required: true
+    }
+  }
+);
+
+mongoose.model('Event', eventSchema, 'events');
