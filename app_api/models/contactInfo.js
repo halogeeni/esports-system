70a6ae70c59a95bb8e57ff7ac7a1251573784b87
Aleksi Rasio <aleksi.rasio@metropolia.fi>
2016-09-15 12:53:20 +0300

    Add contactInfo.js, install mongoose-type-email

---
+++ app_api/models/contactInfo.js
@@ -0,0 +1,41 @@
+/*jslint node: true */
+
+var mongoose = require('mongoose');
+require('mongoose-type-email');
+
+// types should be revised
+
+var contactInfoSchema = new mongoose.Schema(
+  {
+    email: {
+      type: mongoose.SchemaTypes.Email,
+      required: true
+    },
+    streetAddress: {
+      type: String,
+      required: true
+    },
+    postalCode: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    website: {
+      type: String,
+      required: true
+    }
+  }
+);
+
+mongoose.model('ContactInfo', contactInfoSchema);
