c922f9e4548e94884465f4a5b532cd24854fec48
Joel <joel.vainikka@gmail.com>
2016-09-19 10:46:35 +0300

    Added routes file to api

---
+++ app_api/routes/index.js
@@ -0,0 +1,57 @@
+/*jslint node: true */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var ctrlEvents = require('../controllers/events');
+var ctrlGames = require('../controllers/games');
+var ctrlTeams = require('../controllers/teams');
+var ctrlUsers = require('../controllers/users');
+
+// /api/events
+router.get('/events', ctrlEvents.getEvents);
+router.post('/events', ctrlEvents.addEvent);
+
+router.delete('/events/:id', ctrlEvents.deleteEvent);
+router.get('/events/:id', ctrlEvents.getEvent);
+router.put('/events/:id', ctrlEvents.updateEvent);
+
+router.get('/events/:id/tournaments', ctrlEvents.getTournaments);
+router.post('/events/:id/tournaments', ctrlEvents.addTournament);
+
+router.delete('/events/:id/tournaments/:id', ctrlEvents.deleteTournament);
+router.get('/events/:id/tournaments/:id', ctrlEvents.getTournament);
+router.put('/events/:id/tournaments/:id', ctrlEvents.updateTournament);
+
+router.get('/events/:id/tournaments/:id/matches', ctrlEvents.getMatches);
+
+router.delete('/events/:id/tournaments/:id/matches/:id', ctrlEvents.deleteMatch);
+router.get('/events/:id/tournaments/:id/matches/:id', ctrlEvents.getMatch);
+router.put('/events/:id/tournaments/:id/matches/:id', ctrlEvents.updateMatch);
+
+// /api/games
+router.get('/games', ctrlGames.getGames);
+router.post('/games', ctrlGames.addGame);
+
+router.delete('/games/:id', ctrlGames.deleteGame);
+router.get('/games/:id', ctrlGames.getGame);
+router.put('/games/:id', ctrlGames.updateGame);
+
+// /api/teams
+router.get('/teams', ctrlTeams.getTeams);
+router.post('/teams', ctrlTeams.addTeam);
+
+router.delete('/teams/:id', ctrlTeams.deleteTeam);
+router.get('/teams/:id', ctrlTeams.getTeam);
+router.put('/teams/:id', ctrlTeams.updateTeam);
+
+// /api/users
+router.get('/users', ctrlUsers.getUsers);
+router.post('/users', ctrlUsers.addUser);
+
+router.delete('/users/:id', ctrlUsers.deleteUser);
+router.get('/users/:id', ctrlUsers.getUser);
+router.put('/users/:id', ctrlUsers.updateUser);
+
+module.exports = router;
