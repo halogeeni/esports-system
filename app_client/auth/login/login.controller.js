b8f6e306b6f49859e52ad0ef72452f1005648587
Aleksi Rasio <aleksi.rasio@metropolia.fi>
2016-09-29 08:56:56 +0300

    Beautify login.controller.js

--- app_client/auth/login/login.controller.js
+++ app_client/auth/login/login.controller.js
@@ -1,48 +1,48 @@
 (function() {
 
-    angular
-        .module('washbear')
-        .controller('loginCtrl', loginCtrl);
+  angular
+    .module('washbear')
+    .controller('loginCtrl', loginCtrl);
 
-    loginCtrl.$inject = ['$location', 'authentication'];
+  loginCtrl.$inject = ['$location', 'authentication'];
 
-    function loginCtrl($location, authentication) {
-        var vm = this;
+  function loginCtrl($location, authentication) {
+    var vm = this;
 
-        vm.pageHeader = {
-            title: 'Kirjaudu Washbeariin'
-        };
+    vm.pageHeader = {
+      title: 'Kirjaudu Washbeariin'
+    };
 
-        vm.credentials = {
-            email: "",
-            password: ""
-        };
+    vm.credentials = {
+      email: "",
+      password: ""
+    };
 
-        vm.returnPage = $location.search().page || '/';
+    vm.returnPage = $location.search().page || '/';
 
-        vm.onSubmit = function() {
-            vm.formError = "";
-            if (!vm.credentials.email || !vm.credentials.password) {
-                vm.formError = "Vaadittavia tietoja puuttuu";
-                return false;
-            } else {
-                vm.doLogin();
-            }
-        };
+    vm.onSubmit = function() {
+      vm.formError = "";
+      if (!vm.credentials.email || !vm.credentials.password) {
+        vm.formError = "Vaadittavia tietoja puuttuu";
+        return false;
+      } else {
+        vm.doLogin();
+      }
+    };
 
-        vm.doLogin = function() {
-            vm.formError = "";
-            authentication
-                .login(vm.credentials)
-                .error(function(err) {
-                    vm.formError = err;
-                })
-                .then(function() {
-                    $location.search('page', null);
-                    $location.path(vm.returnPage);
-                });
-        };
+    vm.doLogin = function() {
+      vm.formError = "";
+      authentication
+        .login(vm.credentials)
+        .error(function(err) {
+          vm.formError = err;
+        })
+        .then(function() {
+          $location.search('page', null);
+          $location.path(vm.returnPage);
+        });
+    };
 
-    }
+  }
 
 })();
