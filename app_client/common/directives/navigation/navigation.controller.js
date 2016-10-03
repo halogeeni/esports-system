8de9a5db82dcd038f2f4c5c84a1db78598780382
Aleksi Rasio <aleksi.rasio@metropolia.fi>
2016-10-03 14:08:27 +0300

    Implement logged in username display, style tweaks

--- app_client/common/directives/navigation/navigation.controller.js
+++ app_client/common/directives/navigation/navigation.controller.js
@@ -5,7 +5,7 @@
 
   navigationCtrl.$inject = ['$location', 'authentication'];
   function navigationCtrl($location, authentication) {
-    
+
     var vm = this;
 
     vm.currentPath = $location.path();
