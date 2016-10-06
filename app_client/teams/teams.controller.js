8577dbe7c59a53ca56d8b8f6908c5d22ebf28049
Gusgård <gusoskar@hotmail.com>
2016-10-06 14:01:45 +0300

    joel!Tässä mun branch

--- app_client/teams/teams.controller.js
+++ app_client/teams/teams.controller.js
@@ -27,6 +27,43 @@
         return vm.teams;
       });
     }
+    
+    vm.credentials = {
+      name: "",
+      adminUser:"",
+      additionalInfo:""
+      
+    };
+    
+     vm.returnPage = $location.search().page || '/';
+    
+    vm.onSubmit = function() {
+      vm.formError = "";
+      if (!vm.credentials.name ||
+          !vm.credentials.adminuser ||
+          !cm.credentials.additionalInfo)
+          
+      {
+        vm.formError = "Täytä kaikki kentät";
+        return false;
+      } else {
+        vm.doRegister();
+      }
+    };
+    
+    vm.doRegister = function() {
+      vm.formError = "";
+      authentication
+        .register(vm.credentials)
+        .error(function(err) {
+          vm.formError = err;
+        })
+        .then(function() {
+          $location.search('page', null);
+          $location.path(vm.returnPage);
+        });
+    };
+    
 
   }
 
